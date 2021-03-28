3c132eda856788e34729955a735710c7e3d4682f
ChorriCode <compartelo007@yahoo.es>
2021-03-28 18:24:46 +0100

    let's create two integer variables for coordenates x,y

--- src/main/java/com/javier/oop_vs_estructure/MainEstructure.java
+++ src/main/java/com/javier/oop_vs_estructure/MainEstructure.java
@@ -17,22 +17,7 @@ public class MainEstructure {
     public static void main(String[] args) {
 
         // let's create two integer variables for coordenates x,y
-        int x = 1;
-        int y = 0;
-
-        // let's create four coords to do square vertice coords, starting with (0,0)
-        // And after we wil create a method for print coords
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 5;
-        int x3 = 5;
-        int y3 = 5;
-        int x4 = 5;
-        int y4 = 0;
-
-        // let's compact coords into an array for better management
-        // And after we wil create a method for print coords
+       
     }
 
 }
